a70de6cd6880a30e21d252807133cb96e6dc1902
ManeetChhabra <130479331+ManeetChhabra@users.noreply.github.com>
2025-03-22 16:11:10 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Update this URL as needed
+    origin: "https://skill-sensei-frontend.onrender.com", // Update this URL as needed
     credentials: true,
   })
 );
