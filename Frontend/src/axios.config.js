4a8f4446c57ad00fd7b22c06ffd60beec40e6d50
Maneet Singh Chhabra <130479331+ManeetChhabra@users.noreply.github.com>
2025-11-14 16:07:52 +0530

    Update axios baseURL to include API version

--- Frontend/src/axios.config.js
+++ Frontend/src/axios.config.js
@@ -4,6 +4,6 @@ import axios from "axios";
 // Use the Vite environment variable or a fallback value.
 // Make sure your .env file contains: VITE_API_URL=http://localhost:3015/api/v1
 export const api = axios.create({
-  baseURL:"https://skill-sensei-yxti.onrender.com" ,
+  baseURL:"https://skill-sensei-yxti.onrender.com/api/v1" ,
   withCredentials: true, // To send cookies along with requests
 });
